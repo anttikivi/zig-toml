07b027fd121d31f797cdc8fd6481195ca74b5900
Antti Kivi <antti@anttikivi.com>
2026-01-18 09:25:26 +0200

    feat: add an optional diagnostics object for additional information on failed decoding

--- src/decoder.zig
+++ src/decoder.zig
@@ -8,8 +8,35 @@ const DecodeOptions = struct {
     /// Whether to borrow the input string or to copy it to the result type.
     borrow_input: bool = false,
 
+    /// Optional diagnostics object that contains additional information if
+    /// the decoder fails.
+    diagnostics: ?*Diagnostics = null,
+
     /// The version of TOML to accept in the decoding.
     tomlVersion: TomlVersion = .@"1.1.0",
+};
+
+/// Diagnostics can contain additional information about errors in decoding. To
+/// enable diagnostics, initialize the diagnostics object by
+/// `var diagnostics = Diagnostics{};` and pass it with the decoding options:
+/// `const opts = DecodeOptions{ .diagnostics = &diagnostics };`.
+///
+/// The caller must call `deinit` on the diagnostics object. It owns
+/// the `message` string if the decoder has failed, and it is allocated using
+/// the general-purpose allocator that was passed in to `toml.decode`.
+/// The `snippet` points to the same string that the parsed result would point
+/// to.
+const Diagnostics = struct {
+    line: ?usize = null,
+    column: ?usize = null,
+    snippet: ?[]const u8 = null,
+    message: ?[]const u8 = null,
+
+    pub fn deinit(self: *@This(), gpa: Allocator) void {
+        if (self.message) |m| {
+            gpa.free(m);
+        }
+    }
 };
 
 const Parsed = struct {
