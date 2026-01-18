84e31d79d756350c030588980d69d9d42a2aecb5
Antti Kivi <antti@anttikivi.com>
2026-01-18 09:09:02 +0200

    build: clean up creating the module in the build script and add a separate step for running the unit tests

--- build.zig
+++ build.zig
@@ -1,34 +1,48 @@
 const std = @import("std");
 
+const Options = struct {
+    optimize: std.builtin.OptimizeMode,
+    target: std.Build.ResolvedTarget,
+};
+
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    const options: Options = .{
+        .optimize = b.standardOptimizeOption(.{}),
+        .target = b.standardTargetOptions(.{}),
+    };
 
-    const mod = b.addModule("toml", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-    });
-
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-
-    const run_mod_tests = b.addRunArtifact(mod_tests);
+    // Add the library to the package's module set
+    b.modules.put("toml", addTomlMod(b, options)) catch @panic("OOM");
 
     const test_step = b.step("test", "Run all of the tests");
-    test_step.dependOn(&run_mod_tests.step);
+
+    // Unit tests
+    {
+        const step = b.step("test-unit", "Run the unit tests");
+        const tests = b.addTest(.{ .root_module = addTomlMod(b, options) });
+        step.dependOn(&b.addRunArtifact(tests).step);
+        test_step.dependOn(step);
+    }
 
     // Formatting tasks
-
     const fmt_include_paths = &.{"."};
-
     {
         const step = b.step("fmt", "Modify source files in place to have conforming formatting");
         step.dependOn(&b.addFmt(.{ .paths = fmt_include_paths }).step);
     }
-
     {
         const step = b.step("test-fmt", "Check source files having conforming formatting");
         step.dependOn(&b.addFmt(.{ .paths = fmt_include_paths, .check = true }).step);
         test_step.dependOn(step);
     }
+}
+
+fn addTomlMod(b: *std.Build, opts: Options) *std.Build.Module {
+    const mod = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = opts.target,
+        .optimize = opts.optimize,
+    });
+
+    return mod;
 }
