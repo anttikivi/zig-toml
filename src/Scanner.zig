473cca5abf9e2c42fb09036d691234455082dd91
Antti Kivi <antti@anttikivi.com>
2026-01-19 08:40:20 +0200

    feat: add tokenizing of regular strings

--- src/Scanner.zig
+++ src/Scanner.zig
@@ -2,6 +2,7 @@ const Scanner = @This();
 
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const assert = std.debug.assert;
 
 const DecodeOptions = @import("decoder.zig").DecodeOptions;
 const Diagnostics = @import("decoder.zig").Diagnostics;
@@ -24,7 +25,12 @@ diagnostics: ?*Diagnostics = null,
 /// Sentinel character that marks the end of input.
 const end_of_input: u8 = 0;
 
-const Error = Allocator.Error || error{ InvalidControlCharacter, Reported };
+const Error = Allocator.Error || error{
+    InvalidControlCharacter,
+    InvalidEscapeSequence,
+    UnterminatedString,
+    Reported,
+};
 
 /// Feature flags that determine which TOML features that have changed since
 /// 1.0.0 are supported.
@@ -127,7 +133,26 @@ fn next(self: *Scanner, comptime key_mode: bool) Error!Token {
 
                 return .left_bracket;
             },
-            else => return .end_of_file, // TODO: Handle.
+            ']' => {
+                if (key_mode and self.peek() == ']') {
+                    _ = self.nextChar();
+                    return .double_right_bracket;
+                }
+
+                return .right_bracket;
+            },
+            '{' => return .left_brace,
+            '}' => return .right_brace,
+            '"' => {
+                // Move back so that `scanString` finds the first quote.
+                self.cursor -= 1;
+                return self.scanString();
+            },
+            '\'' => {
+                self.cursor -= 1;
+                // return self.scanLiteralString();
+            },
+            else => return .end_of_file, // TODO: Handle literals.
         }
     }
 
@@ -163,7 +188,136 @@ fn peek(self: *const Scanner) u8 {
         return end_of_input;
     }
 
+    assert(self.input[self.cursor] != end_of_input);
+
     return self.input[self.cursor];
+}
+
+fn matchN(self: *const Scanner, c: u8, n: comptime_int) bool {
+    comptime if (n < 2) {
+        @compileError("calling Scanner.matchN with n < 2");
+    };
+
+    assert(c != '\n');
+
+    if (self.cursor + n > self.input.len) {
+        return false;
+    }
+
+    inline for (0..n) |i| {
+        if (self.input[self.cursor + i] != c) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn scanString(self: *Scanner) !Token {
+    assert(self.peek() == '"');
+
+    if (self.matchN('"', 3)) {
+        // TODO: Handle multiline string.
+        return .end_of_file;
+    }
+
+    _ = self.nextChar(); // skip opening quote
+    const start = self.cursor;
+
+    while (self.cursor < self.input.len and self.input[self.cursor] != '"') {
+        var c = self.input[self.cursor];
+        if (c == '\n' or c == '\r') {
+            return self.fail(.{ .err = error.UnterminatedString });
+        }
+
+        if (c == '\\') {
+            self.cursor += 1;
+
+            if (self.cursor >= self.input.len) {
+                return self.fail(.{ .err = error.UnterminatedString });
+            }
+
+            c = self.input[self.cursor];
+
+            switch (c) {
+                // Skip the "normal" escape sequences.
+                '"', '\\', 'b', 'f', 'n', 'r', 't' => self.cursor += 1,
+                // Escape character in TOML 1.1.0.
+                'e' => if (self.features.escape_e) {
+                    self.cursor += 1;
+                } else {
+                    return self.fail(.{ .err = error.InvalidEscapeSequence });
+                },
+                // \xHH for Unicode codepoints < 256.
+                'x' => if (self.features.escape_xhh) {
+                    self.cursor += 1;
+
+                    if (self.cursor + 2 > self.input.len) {
+                        return self.fail(.{ .err = error.InvalidEscapeSequence });
+                    }
+
+                    for (0..2) |_| {
+                        if (!std.ascii.isHex(self.input[self.cursor])) {
+                            return self.fail(.{ .err = error.InvalidEscapeSequence });
+                        }
+
+                        self.cursor += 1;
+                    }
+                } else {
+                    return self.fail(.{ .err = error.InvalidEscapeSequence });
+                },
+                'u' => {
+                    self.cursor += 1;
+
+                    if (self.cursor + 4 > self.input.len) {
+                        return self.fail(.{ .err = error.InvalidEscapeSequence });
+                    }
+
+                    for (0..4) |_| {
+                        if (!std.ascii.isHex(self.input[self.cursor])) {
+                            return self.fail(.{ .err = error.InvalidEscapeSequence });
+                        }
+
+                        self.cursor += 1;
+                    }
+                },
+                'U' => {
+                    self.cursor += 1;
+
+                    if (self.cursor + 8 > self.input.len) {
+                        return self.fail(.{ .err = error.InvalidEscapeSequence });
+                    }
+
+                    for (0..8) |_| {
+                        if (!std.ascii.isHex(self.input[self.cursor])) {
+                            return self.fail(.{ .err = error.InvalidEscapeSequence });
+                        }
+
+                        self.cursor += 1;
+                    }
+                },
+                else => return self.fail(.{ .err = error.InvalidEscapeSequence }),
+            }
+        } else if (isValidChar(c) or c == ' ' or c == '\t') {
+            self.cursor += 1;
+        } else {
+            return self.fail(.{ .err = error.InvalidControlCharacter });
+        }
+    }
+
+    // TODO: Do we compare self.input[self.cursor] != '"' here?
+    if (self.cursor >= self.input.len) {
+        return self.fail(.{ .err = error.UnterminatedString });
+    }
+
+    assert(self.input[self.cursor] == '"');
+
+    const result = self.input[start..self.cursor];
+    self.cursor += 1;
+
+    assert(self.input[self.cursor] != '"');
+
+    return .{ .string = result };
 }
 
 /// Fail the parsing in the Scanner. This either fills the Diagnostics with
@@ -173,6 +327,8 @@ fn fail(self: *const Scanner, opts: struct { err: Error, msg: ?[]const u8 = null
     if (self.diagnostics) |d| {
         const msg = if (opts.msg) |m| m else switch (opts.err) {
             error.InvalidControlCharacter => "invalid control character",
+            error.InvalidEscapeSequence => "invalid escape sequence",
+            error.UnterminatedString => "unterminated string",
             error.Reported => @panic("fail with error.Reported"),
             // OOM should not go through this function.
             error.OutOfMemory => @panic("fail with error.OutOfMemory"),
@@ -183,6 +339,10 @@ fn fail(self: *const Scanner, opts: struct { err: Error, msg: ?[]const u8 = null
     }
 
     return opts.err;
+}
+
+fn isValidChar(c: u8) bool {
+    return std.ascii.isPrint(c) or (c & 0x80) != 0;
 }
 
 test nextKey {
@@ -217,6 +377,28 @@ test nextKey {
                 .end_of_file,
             },
         },
+        .{
+            .input =
+            \\"This is a string"
+            \\
+            ,
+            .seq = &[_]Token{
+                .{ .string = "This is a string" },
+                .line_feed,
+                .end_of_file,
+            },
+        },
+        .{
+            .input =
+            \\"This is \uFFFF a string"
+            \\
+            ,
+            .seq = &[_]Token{
+                .{ .string = "This is \\uFFFF a string" },
+                .line_feed,
+                .end_of_file,
+            },
+        },
     };
 
     for (cases) |case| {
@@ -227,7 +409,13 @@ test nextKey {
 
         for (case.seq) |expected| {
             const actual = try scanner.nextKey();
-            try std.testing.expectEqual(expected, actual);
+            switch (actual) {
+                .string => |actual_str| {
+                    try std.testing.expect(expected == .string);
+                    try std.testing.expectEqualStrings(expected.string, actual_str);
+                },
+                else => try std.testing.expectEqual(expected, actual),
+            }
         }
     }
 }
